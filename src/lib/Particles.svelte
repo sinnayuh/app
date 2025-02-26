aca480e62c48e3e613f51460aec23910382e01aa
sina <92541374+sinnayuh@users.noreply.github.com>
2025-02-26 00:58:43 +0000

    chore: fix particle render on different devices (mobile)

--- src/lib/Particles.svelte
+++ src/lib/Particles.svelte
@@ -122,18 +122,19 @@
 
 <style>
     .particles-container {
-        position: absolute;
+        position: fixed; 
         top: 0;
         left: 0;
-        width: 100%;
-        height: 100%;
+        width: 100vw;
+        height: 100vh;
         background-color: rgba(0, 0, 0, 0.3);
         z-index: 1;
+        overflow: hidden;
     }
 
     canvas {
-        width: 100%;
-        height: 100%;
+        width: 100vw;
+        height: 100vh;
         display: block;
         pointer-events: none;
     }
