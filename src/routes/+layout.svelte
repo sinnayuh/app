283673d0cc241f3e0679cbacb4e7ae5feb7dc8fa
sina <92541374+sinnayuh@users.noreply.github.com>
2025-02-27 22:57:00 +0000

    okay some random internet post said to do this

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,6 @@
 	let { children } = $props();
 </script>
 
-<svelte:head>
-  <style>
-    html, body {
-      scrollbar-width: none !important;
-    }
-    ::-webkit-scrollbar {
-      display: none !important;
-    }
-  </style>
-</svelte:head>
-
 <AnimateTitle />
 
 <div id="portal-container"></div>
